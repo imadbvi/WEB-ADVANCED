0628b288b22612027d105b3a6f157ce10d3c4623
imadbvi <imad.ben.ali.idrissi@student.ehb.be>
2025-05-07 14:34:50 +0200

    zoek functionaliteit en filter
    de twee zouden normaal gezien moeten werken maar allen de zoekfunctionaliteit werkt de filter niet

--- src/js/main.js
+++ src/js/main.js
@@ -1,42 +1,41 @@
-async function fetchAnime(searchTerm = "naruto" , typefilter = "") {
+async function fetchAnime(searchTerm = "naruto", typefilter = "") {
     let url = `https://api.jikan.moe/v4/anime?q=${searchTerm}`;
-if (typefilter !== ""){
-    url += `&type=${typeFilter}`;
-}
+    if (typefilter !== "") {
+        url += `&type=${typeFilter}`;
+    }
     try {
-      const response = await fetch(url);
-      const json = await response.json();
-      const animeList = json.data;
-  
-      const container = document.getElementById("anime-container");
-      container.innerHTML = ""; 
-  
-      animeList.forEach(anime => {
-        const animeItem = document.createElement("div");
-        animeItem.classList.add("anime-item");
-  
-        animeItem.innerHTML = `
+        const response = await fetch(url);
+        const json = await response.json();
+        const animeList = json.data;
+
+        const container = document.getElementById("anime-container");
+        container.innerHTML = "";
+
+        animeList.forEach(anime => {
+            const animeItem = document.createElement("div");
+            animeItem.classList.add("anime-item");
+
+            animeItem.innerHTML = `
           <img src="${anime.images.jpg.image_url}" alt="${anime.title}" />
           <h2>${anime.title}</h2>
           <p>Score: ${anime.score || "Geen score"}</p>
           <p>Type: ${anime.type}</p>
         `;
-  
-        container.appendChild(animeItem);
-      });
-  
-    } catch (error) {
-      console.error("Er is iets misgelopen:", error);
-    }
-  }
-  
-  fetchAnime();
 
-  document.getElementById("search-button").addEventListener("click", () => {
+            container.appendChild(animeItem);
+        });
+
+    } catch (error) {
+        console.error("Er is iets misgelopen:", error);
+    }
+}
+
+fetchAnime();
+
+document.getElementById("search-button").addEventListener("click", () => {
     const searchTerm = document.getElementById("search-input").value;
     const typeFilter = document.getElementById("type-filter").value;
-    fetchAnime(searchTerm,typeFilter);
-  });
+    fetchAnime(searchTerm, typeFilter);
+});
 
 
-  
